31678753edf9216906f00428d06c32409fbb74a2
HinokiSu <yukihinoki@qq.com>
2022-12-09 13:48:47 +0800

    feat: add logs table field and some sql

--- server/src/db/sql/logs-sql.ts
+++ server/src/db/sql/logs-sql.ts
@@ -1,31 +1,33 @@
-import { createSqlField, createSqlValuesMark } from "."
+import { createSqlField, createSqlValuesMark } from '.'
 
+// logs table name
+const tbName = 'logs'
 // logs table
 const fieldOfLogsTBArr = [
-    'id',
-    'log_time',
-    'log_state',
-    'message',
-    'level',
-    'log_type',
-    'time_stamp',
-    'fingerprint',
-    'windows_identity',
-    'machine_name',
-    'file_name',
-    'total_execution_time_in_seconds',
-    'total_execution_time',
-    'initiated_by',
-    'process_name',
-    'process_version',
-    'job_id',
-    'robot_name',
-    'machine_id',
-    'organization_unit_id',
-  ]
-  
-  export const insertLogSql = `INSERT INTO logs (${createSqlField(fieldOfLogsTBArr)}) VALUES (${createSqlValuesMark(
-    fieldOfLogsTBArr
-  )})`
-  
-  
+  'id',
+  'log_time',
+  'log_state',
+  'message',
+  'level',
+  'log_type',
+  'time_stamp',
+  'fingerprint',
+  'windows_identity',
+  'machine_name',
+  'file_name',
+  'total_execution_time_in_seconds',
+  'total_execution_time',
+  'initiated_by',
+  'process_name',
+  'process_version',
+  'job_id',
+  'robot_name',
+  'machine_id',
+  'organization_unit_id',
+]
+
+export const insertLogSql = `INSERT INTO ${tbName} (${createSqlField(
+  fieldOfLogsTBArr
+)}) VALUES (${createSqlValuesMark(fieldOfLogsTBArr)})`
+
+export const deleteLogSql = `DELETE FROM ${tbName} WHERE log_time = strftime('%Y-%m-%d', log_time)`
