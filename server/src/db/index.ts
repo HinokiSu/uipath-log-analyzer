0ceba0d6e869ba119e277dc76b326372fba7e015
HinokiSu <yukihinoki@qq.com>
2022-12-09 13:51:01 +0800

    feat: use better-sqlite and connect db

---
+++ server/src/db/index.ts
@@ -0,0 +1,16 @@
+import Database from 'better-sqlite3'
+import path from 'path'
+const db = new Database(path.join(__dirname, '../../../logs_db.db'))
+db.pragma('journal_mode = WAL')
+
+function query(sql: string, params: any[]) {
+  return db.prepare(sql).all(params)
+}
+
+
+
+function run(sql: string, params: any[]) {
+  return db.prepare(sql).run(params)
+}
+
+export default { run, query }
