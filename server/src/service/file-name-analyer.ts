a704c2a75b19d3311213a02cd256ebe907a33b16
HinokiSu <yukihinoki@qq.com>
2022-12-09 13:51:39 +0800

    feat: add logs file name analyer

---
+++ server/src/service/file-name-analyer.ts
@@ -0,0 +1,43 @@
+import fs from 'fs'
+import { IFileInfoObj } from '../types/log-types'
+const getFilesName = (uipathFolderPath: string): string[] => {
+  // get all files name
+  const filesNameArr = fs.readdirSync(uipathFolderPath, 'utf8').filter((_t: string) => _t.includes('Execution'))
+  return filesNameArr
+}
+
+/**
+ * @param fileName file name(include the extension)
+ * @param rootPath the root path of the uipath logs
+ * @returns Obj is the parsed file name
+ */
+export const getFileObj = (fileName: string, rootPath: string): IFileInfoObj => {
+  // init file obj
+  // {name: is file name, path: whole file path, time: file prefix name}
+  const _obj: IFileInfoObj = {
+    name: '',
+    path: '',
+    time: '',
+  }
+  _obj.name = fileName
+  _obj.path = rootPath.concat('\\', fileName)
+  _obj.time = fileName.split('_Execution')[0]
+  return _obj
+}
+
+/**
+ * @returns parsered file name
+ */
+export const fileNameAnalyer = () => {
+  // TODO: get uipath logs folder path .env config
+  const uipathLogsFolderPath: string = process.env.UIPATH_LOGS_FOLDER_PATH || ''
+  const fileNameArr = getFilesName(uipathLogsFolderPath)
+  const resultArr: IFileInfoObj[] = []
+  // loop to build file obj
+  for (let _fName of fileNameArr) {
+    const fileObj = getFileObj(_fName, uipathLogsFolderPath)
+    resultArr.push(fileObj)
+  }
+  return resultArr
+}
+
