29af4d72e8c28024017b922e3a1ee570366d45df
HinokiSu <yukihinoki@qq.com>
2022-12-19 11:10:27 +0800

    feat: add some deps&modify config

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// pinia
+import { createPinia } from 'pinia'
+// router
+import router from '@routes/index'
+// ant-design-vue manual import
+// import { AntdUIRegister } from '@utils/antd-vue'
 
-createApp(App).mount('#app')
+// pinia
+const pinia = createPinia()
+
+// vue app
+const app = createApp(App)
+
+// ant-design-vue
+// AntdUIRegister(app)
+
+app.use(router)
+app.use(pinia)
+app.mount('#app')
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from 'path'
+// modular import plugin
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Components({
+      // ant-design-vue 
+      resolvers: [AntDesignVueResolver()]
+    })
+  ],
+  resolve: {
+    alias: [
+      { find: '@views', replacement: resolve(__dirname, 'src/views') },
+      { find: '@components', replacement: resolve(__dirname, 'src/components') },
+      { find: '@routes', replacement: resolve(__dirname, 'src/routes') },
+      { find: '@stores', replacement: resolve(__dirname, 'src/stores') },
+      { find: '@interface', replacement: resolve(__dirname, 'src/interface') },
+      { find: '@utils', replacement: resolve(__dirname, 'src/utils') },
+      { find: '@plugin', replacement: resolve(__dirname, 'src/plugin') },
+      { find: '@api', replacement: resolve(__dirname, 'src/api') }
+    ]
+  }
 })
