7d29957b14283677cdd70ad2cebe93f7335da600
HinokiSu <yukihinoki@qq.com>
2022-12-19 11:08:55 +0800

    feat: add menu related sub componets

---
+++ app/src/components/menu/menu-icon.vue
@@ -0,0 +1,33 @@
+<template>
+  <component :is="name" :style="style" :class="className" />
+</template>
+
+<script lang="ts">
+import { DotChartOutlined, FolderOutlined, FileTextOutlined } from '@ant-design/icons-vue'
+import { defineComponent } from 'vue'
+// refer: https://github.com/ant-design/ant-design/issues/20668
+export default defineComponent({
+  name: 'MenuIcon',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    className: {
+      type: String,
+      default: ''
+    },
+    style: {
+      type: Object,
+      default: () => ({
+        fontSize: '20px'
+      })
+    }
+  },
+  components: {
+    DotChartOutlined,
+    FolderOutlined,
+    FileTextOutlined
+  }
+})
+</script>
---
+++ app/src/components/menu/menu-item.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- menu item -->
+  <a-menu-item :key="menuItem.key" v-if="!menuItem.children">
+    <template #icon>
+      <menu-icon :name="menuItem.icon"></menu-icon>
+    </template>
+    <span>{{ menuItem.title }}</span>
+  </a-menu-item>
+  <!-- Menu has sub menu item -->
+  <template v-else>
+    <a-sub-menu :key="menuItem.key">
+      <template #icon>
+        <!-- <menu-icon :icon="menuItem.icon" /> -->
+      </template>
+      <template #title>{{ menuItem.title }}</template>
+
+      <div v-for="item in menuItem.children" :key="item.key">
+        <a-menu-item :key="item.key">
+          {{ item.title }}
+        </a-menu-item>
+      </div>
+    </a-sub-menu>
+  </template>
+</template>
+
+<script lang="ts">
+import { defineComponent, h, PropType, Ref, ref } from 'vue'
+import { MenuItemType } from '@interface/menu'
+import MenuIcon from './menu-icon.vue'
+export default defineComponent({
+  name: 'MenuItem',
+  props: {
+    menuItem: Object as PropType<MenuItemType>
+  },
+  components: {
+    MenuIcon
+  },
+
+  setup(props) {
+    const menuItem = ref(props.menuItem) as Ref<MenuItemType>
+
+    return {
+      menuItem
+    }
+  }
+})
+</script>
+
+<style scoped></style>
