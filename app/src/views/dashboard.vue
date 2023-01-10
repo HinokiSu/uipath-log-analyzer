34928b4d6463ffacfe4939df2813225827f9927e
HinokiSu <yukihinoki@qq.com>
2023-01-10 14:57:40 +0800

    feat: filling dashboard and log file pages

--- app/src/views/dashboard.vue
+++ app/src/views/dashboard.vue
@@ -1,18 +1,74 @@
 <template>
-  <div>
-    dashboard
+  <div class="dashboard__wrapper">
+    <recently-error-card></recently-error-card>
+    <a-card class="custom-card log-state_card__wrapper" title="日志状态" :bordered="false">
+      <log-state-chart></log-state-chart>
+    </a-card>
+
+    <a-card class="custom-card log-time_card__warpper" title="时间&日志状态" :bordered="false">
+      <log-time-chart></log-time-chart>
+    </a-card>
   </div>
 </template>
 
 <script lang="ts">
+import { useStatsStore } from '@/stores/stats-store'
 import { defineComponent } from 'vue'
-
+import LogTimeChart from '@components/charts/log-time-stats/log-time-chart.vue'
+import LogStateChart from '@components/charts/log-state-stats/log-state-chart.vue'
+import RecentlyErrorCard from '@components/recently-err-card/index.vue'
 export default defineComponent({
   name: 'Dashboard',
+  components: {
+    LogTimeChart,
+    LogStateChart,
+    RecentlyErrorCard
+  },
   setup() {
+    const statsStore = useStatsStore()
+
     return {}
   }
 })
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.dashboard__wrapper {
+  width: 100%;
+  background: #fafbfb;
+  padding: 20px;
+  // display: flex;
+  // flex-wrap: wrap;
+
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+
+  @media (max-width: 900px) {
+    grid-template-columns: 1fr;
+  }
+
+  .custom-card {
+    background-color: rgb(255, 255, 255);
+    color: rgba(0, 0, 0, 0.87);
+    transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    overflow: hidden;
+    border-radius: 20px;
+    padding: 14px;
+    margin: 15px;
+    box-shadow: rgba(90, 114, 123, 0.11) 0px 7px 30px 0px;
+  }
+
+  .log-time_card__warpper {
+    width: 100%;
+    min-width: 400px;
+    overflow: auto;
+  }
+
+  .log-state_card__wrapper {
+    max-width: 500px;
+    max-height: 400px;
+    min-width: 400px;
+    min-height: 300px;
+  }
+}
+</style>
--- app/src/views/logs-file.vue
+++ app/src/views/logs-file.vue
@@ -3,7 +3,12 @@
     <a-table :dataSource="dataSource" :columns="columns" :pagination="false" :loading="loading">
       <template #bodyCell="{ column, text, record }">
         <template v-if="column.dataIndex === 'operation'">
-          <a @click="onViewDetail(record.id)">View</a>
+          <a @click="onViewDetail(record.time)">View</a>
+          <a-popconfirm title="是否解析" @confirm="confirmOfPop(record.id)" @cancel="cancelOfPop">
+            <a-button style="margin-left: 20px" type="primary" :disabled="btnDisabled">
+              解析
+            </a-button>
+          </a-popconfirm>
         </template>
       </template>
     </a-table>
@@ -20,53 +25,60 @@
 <script lang="ts">
 import { computed, defineComponent, onMounted, ref, watch } from 'vue'
 import { useLogsFileStore } from '@stores/logs-file-store'
-import { TableColumnsType } from '@interface/logs-file'
+import { TableColumnsType } from '@/interface/common-type'
+import { fetchParseSpecifyFile } from '@/api/parse-api'
+import msg from '@/utils/message'
+import { useRouter } from 'vue-router'
+import { useMenuStore } from '@/stores/menu-store'
 
 export default defineComponent({
   name: 'LogsFile',
   setup() {
     const logsFileStore = useLogsFileStore()
     const pagination = computed(() => logsFileStore.pagination)
-
+    const router = useRouter()
     const dataSource = computed(() => logsFileStore.infoList)
     const loading = ref(false)
+    const btnDisabled = ref(false)
     const columns: TableColumnsType[] = [
       {
         title: '文件名称',
         dataIndex: 'file_name',
-        width: '25%'
+        width: '30%'
       },
       {
         title: '是否解析',
         dataIndex: 'is_parsed',
-        width: '15%'
+        width: '10%'
       },
       {
         title: '创建时间',
         dataIndex: 'created_at',
-        width: '25%'
+        width: '20%'
       },
       {
-        title: '最近更新时间',
+        title: '最近解析时间',
         dataIndex: 'updated_at',
-        width: '25%'
+        width: '20%'
       },
       {
         title: '操作',
         dataIndex: 'operation',
-        width: '10%'
+        width: '20%'
       }
     ]
+    const menuStore = useMenuStore()
+
+    const getDataAndWait = () => {
+      loading.value = true
+      logsFileStore.getLogsFileInfo().then(() => {
+        loading.value = false
+      })
+    }
 
     onMounted(() => {
-      logsFileStore.getTotal()
-      logsFileStore.getLogsFileInfo()
+      getDataAndWait()
     })
-
-    watch(
-      () => pagination.value.curPage,
-      (newVal) => {}
-    )
 
     const changeCurrentPage = (page: number, pageSize: number) => {
       loading.value = true
@@ -77,20 +89,57 @@ export default defineComponent({
     }
 
     const onViewDetail = (val: string) => {
-      // redirect logs table page
-      console.log(val)
+      // redirect logs time table page
+      router.push({
+        name: 'logsTime',
+        query: {
+          time: val
+        }
+      })
+      menuStore.updateSelectedKeys(['3.2'])
     }
 
-    return { columns, dataSource, pagination, loading, changeCurrentPage, onViewDetail }
+    const confirmOfPop = (id: string) => {
+      btnDisabled.value = true
+      console.log(id)
+      msg.info('正在解析...')
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          fetchParseSpecifyFile(id).then(() => {
+            btnDisabled.value = false
+            msg.ok('解析成功')
+            getDataAndWait()
+            resolve(true)
+          })
+        }, 3000)
+      })
+    }
+
+    const cancelOfPop = () => {}
+
+    return {
+      columns,
+      dataSource,
+      pagination,
+      loading,
+      changeCurrentPage,
+      onViewDetail,
+      confirmOfPop,
+      cancelOfPop,
+      btnDisabled
+    }
   }
 })
 </script>
 
 <style lang="less" scoped>
-.paging-container {
-  margin-top: 16px;
-  padding-bottom: 24px;
-  display: flex;
-  place-content: center;
+.logs-file-info_wrapper {
+  
+  .paging-container {
+    margin-top: 30px;
+    padding-bottom: 24px;
+    display: flex;
+    place-content: center;
+  }
 }
 </style>
