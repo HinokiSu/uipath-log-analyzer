754e8cd49fb79dd69bc71c1edc6d5eb418906e11
HinokiSu <yukihinoki@qq.com>
2023-01-10 14:59:15 +0800

    chore: use echarts and global style

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { provide } from 'vue'
+import * as echarts from 'echarts/core'
 import layout from './layout/layout.vue'
+provide('echarts', echarts)
 </script>
 <template>
-    <layout></layout>
+  <layout></layout>
 </template>
 
 <style scoped></style>
