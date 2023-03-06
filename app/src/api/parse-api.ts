374dcd47cf8a28c9e33a9880ae4a9469b28b6da8
HinokiSu <yukihinoki@qq.com>
2023-03-06 10:49:03 +0800

    feat: add parse log files by range-date api&store

--- app/src/api/parse-api.ts
+++ app/src/api/parse-api.ts
@@ -9,8 +9,18 @@ export const fetchParseSpecifyFile = (id: string): Promise<TBaseResult> =>
   })
 
 export const fetchParseAllLogFileInfo = (): Promise<TBaseResult> =>
-  ApiClient.get(apiURLHandler(apiBasePath, `/fileinfo/all`))
+  ApiClient.get(apiURLHandler(apiBasePath, `/file/info/all`))
 
 // parse all log by all log files
 export const fetchParseLogsByAllLogFile = (): Promise<TBaseResult> =>
   ApiClient.get(apiURLHandler(apiBasePath, `/file/all`))
+
+// parse by range date
+export const fetchParseLogsByRangeDateAndLogFile = (
+  start: string,
+  end: string
+): Promise<TBaseResult> =>
+  ApiClient.get(apiURLHandler(apiBasePath, `/file/date`), {
+    start,
+    end
+  })
